e44b6556e2476aec055b16ae65de1629ebe1ca0b
Ganga-sharma106 <ganga.sharma1800@gmail.com>
2025-03-11 17:09:27 +0530

    Updated the file with new changes

--- home/views.py
+++ home/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render , HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
     # context={
     #     'value1': 'this is my first value'
     # }
+    # messages.success(request,"This is a test message.")
     return render(request,"index.html")
     # return HttpResponse("This is Home Page")
     
@@ -18,5 +23,14 @@ def services(request):
     # return HttpResponse("This is Service Page")
 
 def contact(request):
-     return render(request,"contact.html")
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date = datetime.today())
+        contact.save() 
+        messages.success(request, "Your message has been sent!")  
+    return render(request,"contact.html")
     # return HttpResponse("This is Contact Page")
+ 
